58346fc4cbf8498ac84c558c5a7c2079ec679dea
FatemaAkter11 <fatemaakter4261@gmail.com>
2021-09-26 00:25:59 +0600

    fake data created and header component added

---
+++ src/fakeData/programmers.js
@@ -0,0 +1,90 @@
+const programmers = [
+    {
+        key: "B001R",
+        name: "David Patterson",
+        age: 73,
+        achievement: "Eckert–Mauchly Award, Karl Karlstrom, Outstanding Educator, and ACM Distinguished Service Award",
+        salary: 39375,
+        img: "https://i0.wp.com/www.rankred.com/wp-content/uploads/2019/07/David-Patterson.jpg?w=700&ssl=1"
+    },
+    {
+        key: "B002R",
+        name: "John Carmack",
+        age: 51,
+        achievement: "The National Academy of Television Arts & Sciences (twice), the Technology & Engineering Emmy Awards, and Game Developers Conference Lifetime Achievement award for his pioneering work",
+        salary: 13000,
+        img: "https://i0.wp.com/www.rankred.com/wp-content/uploads/2019/07/David-Patterson.jpg?w=700&ssl=1"
+    },
+    {
+        key: "B003R",
+        name: "Donald Ervin Knuth",
+        age: 83,
+        achievement: "Turing Award, Grace Murray Hopper Award, National Medal of Science, Faraday Medal, Kyoto Prize, and John von Neumann Medal",
+        salary: 33000,
+        img: "https://i0.wp.com/www.rankred.com/wp-content/uploads/2019/07/Donald-Ervin-Knuth.png?w=700&ssl=1"
+    },
+    {
+        key: "B004R",
+        name: "Guido van Rossum",
+        age: 65,
+        achievement: "Rossum received an NLUUG Award, the 2001 Award for the Advancement of Free Software for his work on Python",
+        salary: 23000,
+        img: "https://i2.wp.com/www.rankred.com/wp-content/uploads/2019/07/Guido-van-Rossum.jpg?w=700&ssl=1"
+    },
+    {
+        key: "B005R",
+        name: "James Arthur Gosling",
+        age: 66,
+        achievement: "Gosling was awarded The Economist Innovation Award,  IEEE John von Neumann Medal, and he was made an Officer of the Order of Canada in 2007",
+        salary: 15000,
+        img: "https://i2.wp.com/www.rankred.com/wp-content/uploads/2019/07/James-Arthur-Gosling.jpg?w=700&ssl=1"
+    },
+    {
+        key: "B006R",
+        name: " Niklaus Wirth",
+        age: 87,
+        achievement: " He was honored with Turing Award, SIGPLAN Programming Languages Achievement Award, and Marcel Benoist Prize",
+        salary: 39000,
+        img: "https://i1.wp.com/www.rankred.com/wp-content/uploads/2019/07/Niklaus-Wirth.jpg?w=700&ssl=1"
+    },
+    {
+        key: "B007R",
+        name: "Alan Curtis Kay",
+        age: 81,
+        achievement: "Alan received ACM Turing Award for his work on object-oriented programming, UPE Abacus Award, and UdK 01-Award  for pioneering the GUI",
+        salary: 73000,
+        img: "https://i2.wp.com/www.rankred.com/wp-content/uploads/2019/07/Alan-Curtis-Kay.jpg?w=700&ssl=1"
+    },
+    {
+        key: "B008R",
+        name: "John McCarthy",
+        age: 84,
+        achievement: "He was honored with the Turing Award, Kyoto Prize, National Medal of Science, Computer Pioneer Award, and IJCAI Award for Research Excellence",
+        salary: 43000,
+        img: "https://i2.wp.com/www.rankred.com/wp-content/uploads/2019/07/John-McCarthy.jpg?w=700&ssl=1"
+    },
+    {
+        key: "B009R",
+        name: "Thomas Eugene Kurtz",
+        age: 93,
+        achievement: "Kurtz was honored with AFIPS Pioneer Award and IEEE Computer Science Pioneer Award",
+        salary: 22600,
+        img: "https://i1.wp.com/www.rankred.com/wp-content/uploads/2019/07/Thomas-Eugene-Kurtz.jpg?w=700&ssl=1"
+    },
+    {
+        key: "B0010R",
+        name: "John George Kemeny",
+        age: 66,
+        achievement: "He received the Computer Pioneer Award in 1985",
+        salary: 55000,
+        img: "https://i2.wp.com/www.rankred.com/wp-content/uploads/2019/07/John-George-Kemeny.jpg?w=700&ssl=1"
+    },
+    {
+        key: "B0011R",
+        name: "Grace Hopper",
+        age: 85,
+        achievement: "She was the first woman in the world who got her name in Distinguished Fellow of the British Computer Society, Honorary Doctor of Science from Marquette University, and National Medal of Technology (1991)",
+        salary: 46900,
+        img: "https://i0.wp.com/www.rankred.com/wp-content/uploads/2019/07/Grace-Hopper.jpg?w=700&ssl=1"
+    }
+]
