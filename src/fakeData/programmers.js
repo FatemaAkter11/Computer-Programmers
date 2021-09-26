f8dc9fdeb1cbdde16211722c337ad1524093b3ff
FatemaAkter11 <fatemaakter4261@gmail.com>
2021-09-26 10:17:35 +0600

    json file update

--- src/fakeData/programmers.js
+++ src/fakeData/programmers.js
@@ -3,6 +3,7 @@ const programmers = [
         key: "B001R",
         name: "David Patterson",
         age: 73,
+        country: "America",
         achievement: "Eckert–Mauchly Award, Karl Karlstrom, Outstanding Educator, and ACM Distinguished Service Award",
         salary: 39375,
         img: "https://i0.wp.com/www.rankred.com/wp-content/uploads/2019/07/David-Patterson.jpg?w=700&ssl=1"
@@ -11,6 +12,7 @@ const programmers = [
         key: "B002R",
         name: "John Carmack",
         age: 51,
+        country: "America",
         achievement: "The National Academy of Television Arts & Sciences (twice), the Technology & Engineering Emmy Awards, and Game Developers Conference Lifetime Achievement award for his pioneering work",
         salary: 13000,
         img: "https://i0.wp.com/www.rankred.com/wp-content/uploads/2019/07/David-Patterson.jpg?w=700&ssl=1"
@@ -19,6 +21,7 @@ const programmers = [
         key: "B003R",
         name: "Donald Ervin Knuth",
         age: 83,
+        country: "America",
         achievement: "Turing Award, Grace Murray Hopper Award, National Medal of Science, Faraday Medal, Kyoto Prize, and John von Neumann Medal",
         salary: 33000,
         img: "https://i0.wp.com/www.rankred.com/wp-content/uploads/2019/07/Donald-Ervin-Knuth.png?w=700&ssl=1"
@@ -27,6 +30,7 @@ const programmers = [
         key: "B004R",
         name: "Guido van Rossum",
         age: 65,
+        country: "Netherlands",
         achievement: "Rossum received an NLUUG Award, the 2001 Award for the Advancement of Free Software for his work on Python",
         salary: 23000,
         img: "https://i2.wp.com/www.rankred.com/wp-content/uploads/2019/07/Guido-van-Rossum.jpg?w=700&ssl=1"
@@ -35,14 +39,16 @@ const programmers = [
         key: "B005R",
         name: "James Arthur Gosling",
         age: 66,
+        country: "Canada",
         achievement: "Gosling was awarded The Economist Innovation Award,  IEEE John von Neumann Medal, and he was made an Officer of the Order of Canada in 2007",
         salary: 15000,
         img: "https://i2.wp.com/www.rankred.com/wp-content/uploads/2019/07/James-Arthur-Gosling.jpg?w=700&ssl=1"
     },
     {
         key: "B006R",
-        name: " Niklaus Wirth",
+        name: "Niklaus Wirth",
         age: 87,
+        country: "Switzerland",
         achievement: " He was honored with Turing Award, SIGPLAN Programming Languages Achievement Award, and Marcel Benoist Prize",
         salary: 39000,
         img: "https://i1.wp.com/www.rankred.com/wp-content/uploads/2019/07/Niklaus-Wirth.jpg?w=700&ssl=1"
@@ -51,6 +57,7 @@ const programmers = [
         key: "B007R",
         name: "Alan Curtis Kay",
         age: 81,
+        country: "America",
         achievement: "Alan received ACM Turing Award for his work on object-oriented programming, UPE Abacus Award, and UdK 01-Award  for pioneering the GUI",
         salary: 73000,
         img: "https://i2.wp.com/www.rankred.com/wp-content/uploads/2019/07/Alan-Curtis-Kay.jpg?w=700&ssl=1"
@@ -59,6 +66,7 @@ const programmers = [
         key: "B008R",
         name: "John McCarthy",
         age: 84,
+        country: "America",
         achievement: "He was honored with the Turing Award, Kyoto Prize, National Medal of Science, Computer Pioneer Award, and IJCAI Award for Research Excellence",
         salary: 43000,
         img: "https://i2.wp.com/www.rankred.com/wp-content/uploads/2019/07/John-McCarthy.jpg?w=700&ssl=1"
@@ -67,6 +75,7 @@ const programmers = [
         key: "B009R",
         name: "Thomas Eugene Kurtz",
         age: 93,
+        country: "America",
         achievement: "Kurtz was honored with AFIPS Pioneer Award and IEEE Computer Science Pioneer Award",
         salary: 22600,
         img: "https://i1.wp.com/www.rankred.com/wp-content/uploads/2019/07/Thomas-Eugene-Kurtz.jpg?w=700&ssl=1"
@@ -75,6 +84,7 @@ const programmers = [
         key: "B0010R",
         name: "John George Kemeny",
         age: 66,
+        country: "America",
         achievement: "He received the Computer Pioneer Award in 1985",
         salary: 55000,
         img: "https://i2.wp.com/www.rankred.com/wp-content/uploads/2019/07/John-George-Kemeny.jpg?w=700&ssl=1"
@@ -83,8 +93,54 @@ const programmers = [
         key: "B0011R",
         name: "Grace Hopper",
         age: 85,
+        country: "America",
         achievement: "She was the first woman in the world who got her name in Distinguished Fellow of the British Computer Society, Honorary Doctor of Science from Marquette University, and National Medal of Technology (1991)",
         salary: 46900,
         img: "https://i0.wp.com/www.rankred.com/wp-content/uploads/2019/07/Grace-Hopper.jpg?w=700&ssl=1"
+    },
+    {
+        key: "B0012R",
+        name: "John Backus",
+        age: 82,
+        country: "America",
+        achievement: "Backus received the W.W. McDowell Award, National Medal of Science, ACM Turing Award, Draper Prize, and named an IBM Fellow",
+        salary: 17500,
+        img: "https://i2.wp.com/www.rankred.com/wp-content/uploads/2019/07/John-Backus.jpg?w=700&ssl=1"
+    },
+    {
+        key: "B0013R",
+        name: "Bill Gates",
+        age: 65,
+        country: "America",
+        achievement: "He was honored with the National Medal of Technology and Innovation, Distinguished Fellow of the British Computer Society, and Bower Award for Business Leadership",
+        salary: 27500,
+        img: "https://i0.wp.com/www.rankred.com/wp-content/uploads/2019/07/Bill-Gates.jpg?w=700&ssl=1"
+    },
+    {
+        key: "B0014R",
+        name: "Brian Kernighan",
+        age: 79,
+        country: "Canada",
+        achievement: "He won INFORMS Computing Society Prize in 1993 and received many Teacher Awards throughout his career",
+        salary: 15000,
+        img: "https://i2.wp.com/www.rankred.com/wp-content/uploads/2019/07/Brian-Kernighan.jpg?w=700&ssl=1"
+    },
+    {
+        key: "B0015R",
+        name: "Ken Thompson",
+        age: 78,
+        country: "America",
+        achievement: "He was honored with the Turing Award, Japan Prize, IEEE Emanuel R. Piore Award, Computer Pioneer Award, and National Medal of Technology",
+        salary: 27000,
+        img: "https://i1.wp.com/www.rankred.com/wp-content/uploads/2019/07/Ken-Thompson.jpg?w=600&ssl=1"
+    },
+    {
+        key: "B0016R",
+        name: "Linus Torvalds",
+        age: 51,
+        country: "Finland",
+        achievement: "He was honored with the Millennium Technology Prize, along with Shinya Yamanaka, for creating an open-source operating system. He also received an EFF Pioneer Award, Lovelace Medal from the British Computer Society, and Vollum Award from Reed College",
+        salary: 7500,
+        img: "https://i2.wp.com/www.rankred.com/wp-content/uploads/2019/07/Linus-Torvalds.jpg?w=700&ssl=1"
     }
 ]
